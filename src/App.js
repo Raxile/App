a1e1a867b65972ed5a3bc260a638467a0a15590f
Raxile <princerocks889@gmail.com>
2022-11-23 21:26:49 +0530

    Section4 complete

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{ useCallback,useState } from "react";
 import {
   BrowserRouter as Router,
   Routes,
@@ -9,27 +9,54 @@ import NewPlace from "./places/pages/NewPlace";
 import UpdatePlace from "./places/pages/UpdatePlace";
 import UserPlaces from "./places/pages/UserPlaces";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import { AuthContext } from "./shared/context/auth-context";
 import Auth from "./user/pages/Auth";
 import Users from "./user/pages/Users";
 
 const App = () => {
-  return (
-    <>
-      <Router>
-        <MainNavigation />
-        <main>
-         <Routes>
+  const [isLoggedIn,setIsLoggedIn] = useState(false);
+
+  const login = useCallback(()=>{
+    setIsLoggedIn(true);
+  },[]);
+
+  const logout = useCallback(()=>{
+    setIsLoggedIn(false);
+  },[]); 
+
+  let routes;
+
+  if(isLoggedIn){
+    routes = (
+      <Routes>
             <Route exact path="/" element={ <Users />} />
             <Route path="/:userId/places" element={<UserPlaces />} />
             <Route exact path="/places/new" element={ <NewPlace />} />
             <Route path="/places/:placeId" element={<UpdatePlace />}  />
-            <Route path="/auth" element={<Auth / >} />
             <Route path="*" element={<Navigate to="/"/>} />
-    
-         </Routes>
+      </Routes>
+    );
+  } else {
+    routes =(
+      <Routes>
+          <Route exact path="/" element={ <Users />} />
+          <Route path="/:userId/places" element={<UserPlaces />} />
+          <Route path="/auth" element={<Auth / >} />
+          <Route path="*" element={<Navigate to="/auth"/>} />
+      </Routes>
+    );
+  }
+
+
+  return (
+    <AuthContext.Provider value={{isLoggedIn:isLoggedIn,login:login,logout:logout}}>
+      <Router>
+        <MainNavigation />
+        <main>
+         {routes}
          </main> 
       </Router>   
-    </>
+    </AuthContext.Provider>
   );
 }
 
--- src/shared/components/Navigation/NavLinks.js
+++ src/shared/components/Navigation/NavLinks.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { NavLink} from 'react-router-dom'
+import { AuthContext } from '../../context/auth-context';
 import './NavLinks.css'
 
 const NavLinks = (props) => {
+   const auth = useContext(AuthContext);
+
+
   return (
     
      <ul className='nav-links'>
@@ -10,17 +14,28 @@ const NavLinks = (props) => {
        <NavLink exact="true" to="/" >ALL USERS</NavLink> 
      </li>
        {/* RENDER WHEN LOGED IN */}
-      <li>
+     {auth.isLoggedIn && (
+     <li>
        <NavLink to="/u1/places">MY PLACES</NavLink> 
-     </li>  
+     </li>
+     )}  
      {/* RENDER WHEN LOGED IN */}
+      {auth.isLoggedIn &&(
       <li>
        <NavLink to="/places/new">ADD PLACES </NavLink> 
-     </li>  
+     </li>
+     )}
      {/* RENDER WHEN NOT LOGED IN */}
+      {!auth.isLoggedIn &&(
       <li>
        <NavLink to="/auth">AUTHENTICATE</NavLink> 
      </li>  
+     )}
+     {auth.isLoggedIn &&(
+      <li>
+        <button onClick={auth.logout}>LOGOUT</button>
+      </li>
+     )}
     </ul> 
     
   )

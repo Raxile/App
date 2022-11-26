1c88f3766b26d6093dd151ce3ee3ebec66cc5981
Raxile <princerocks889@gmail.com>
2022-11-26 19:49:19 +0530

    v99 complete

---
+++ backend/controllers/places-controllers.js
@@ -0,0 +1,92 @@
+const {v4:uuidv4} = require('uuid');
+
+const HttpError = require('../models/http-error');
+
+let DUMMY_PLACES =[
+    {
+      id:'p1',
+      title:'Zsquare',
+      description:'building',
+      address:'Mall Rd, Downtown, Kanpur, Uttar Pradesh 208001',
+      location:{
+        lat:26.4732591,
+        lng:80.3528144
+      },
+      creator:'u1'
+  
+    },
+    
+  ];
+
+const getPlaceById = (req,res,next)=>{
+    const placeid = req.params.pid;
+    const place = DUMMY_PLACES.find(p=>{
+        return p.id === placeid;
+     });
+    
+     if(!place){
+         throw new HttpError('could not find a place for the provider user id',404);
+     }
+
+         res.json({place}); // => { place } => {place : place}
+     
+};
+
+// function getPlaceById () {...}
+//const getPlaceById = function() {...}
+
+const  getPlaceByUserId = (req,res,next)=>{
+    const userid = req.params.uid;
+    const place = DUMMY_PLACES.find(p=>{
+        return p.creator === userid
+    }); 
+    
+    if(!place){
+        return next(new HttpError('could not find a place for the creater id',404));
+      }
+    res.json ({ place });
+};
+
+const createPlace = (req,res,next)=>{
+  const {title,description,coordinates,address,creator} = req.body;
+  //const title = req.body.title;
+  const createdPlace ={
+    id:uuidv4(),
+    title,
+    description,
+    location:coordinates,
+    address,
+    creator
+  };
+
+  DUMMY_PLACES.push(createdPlace);  //unshift(createdPlace)
+  
+  res.status(201).json({place:createdPlace})
+};
+
+const updatePlace = (req,res,next)=>{
+  const {title,description} = req.body;
+  const placeid = req.params.pid;
+
+  const updatePlace ={ ...DUMMY_PLACES.find(p=> p.id === placeid)};
+  const placeIndex = DUMMY_PLACES.findIndex(p=> p.id === placeid)
+  updatePlace.title = title;
+  updatePlace.description = description;
+  DUMMY_PLACES[placeIndex] = updatePlace;
+
+  res.status(200).json({place:updatePlace})
+
+};
+
+const deletePlace = (req,res,next)=>{
+    const placeid = req.params.pid;
+    DUMMY_PLACES = DUMMY_PLACES.filter(p=>p.id !== placeid );
+    res.status(200).json({message:'delete place.'});
+
+};
+
+exports.getPlaceById = getPlaceById;
+exports.getPlaceByUserId = getPlaceByUserId;
+exports.createPlace = createPlace;
+exports.updatePlace = updatePlace;
+exports.deletePlace =deletePlace;
--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -1,51 +1,17 @@
 const express = require('express');
 
+const placeControllers = require('../controllers/places-controllers');
+
 const router = express.Router();
-
-const DUMMY_PLACES =[
-    {
-      id:'p1',
-      title:'Zsquare',
-      description:'building',
-      address:'Mall Rd, Downtown, Kanpur, Uttar Pradesh 208001',
-      location:{
-        lat:26.4732591,
-        lng:80.3528144
-      },
-      creator:'u1'
-  
-    },
-    
-  ]
  
-router.get('/:pid',(req,res,next)=>{
-    const placeid = req.params.pid;
-    const place = DUMMY_PLACES.find(p=>{
-        return p.id === placeid;
-     });
-    
-     if(!place){
-        const error = new Error('could not find a place for the provider user id');
-         error.code = 404;
-         throw error;
-     }
+router.get('/:pid',placeControllers.getPlaceById);
 
-         res.json({place}); // => { place } => {place : place}
-     
-});
+router.get('/user/:uid',placeControllers.getPlaceByUserId);
 
-router.get('/user/:uid',(req,res,next)=>{
-    const userid = req.params.uid;
-    const place = DUMMY_PLACES.find(p=>{
-        return p.creator === userid
-    }); 
-    
-    if(!place){
-        const error = new Error('could not find a place for the creater id');
-         error.code = 404;
-        return next(error);
-      }
-    res.json ({ place });
-});
+router.post('/',placeControllers.createPlace);
+
+router.patch('/:pid',placeControllers.updatePlace);
+
+router.delete('/:pid',placeControllers.deletePlace);
 
 module.exports = router;

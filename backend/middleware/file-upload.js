04ca00f86afdc83a8c2b92dabc71a52c846c6a36
Raxile <princerocks889@gmail.com>
2022-12-14 13:22:32 +0530

    n

--- backend/middleware/file-upload.js
+++ backend/middleware/file-upload.js
@@ -17,13 +17,13 @@ const fileUpload  = multer({
             const ext = MIME_TYPE_MAP[file.mimetype];
             cb(null,uuidv1() + '.' + ext);
         },
+    }),
         fileFilter: (req,file,cb)=>{
             const isValid = !!MIME_TYPE_MAP[file.mimetype];
             let error = isValid ? null :new Error('Invalid mime type');
             cb(error , isValid);
 
         }
-    })
 });
 
 module.exports = fileUpload;
